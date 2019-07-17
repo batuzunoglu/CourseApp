13240346d34282aca0fc508561aa9879c0a8c5f9
batuzunoglu <batuzunoglu@gmail.com>
2019-07-17 17:17:40 +0300

    v1_1
    las 3 videos

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,32 +1,54 @@
+using System.Linq;
 using CourseApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CourseApp.Controllers{
+namespace CourseApp.Controllers
+{
 
-        public class CourseController:Controller
+    // localhost:5000/course
+    public class CourseController : Controller
+    {
+        // action method
+        // localhost:5000/course/index => course/index.cshtml
+        public IActionResult Index()
         {
-            public IActionResult Index(){
-                return View();
-            }
-
-            public IActionResult Apply()
-            {
-              return View();
-            }
-
-            public IActionResult Details(){
-                var course = new Course();
-                course.Name = "Core Mvc Kursu";
-                course.description = "güzel bir kurs";
-                course.isPublished = true;
-                
-                return View(course);
-            }
-
-             public IActionResult List(){
-                return View();
-            }
-
-
+            return View();
         }
+
+        // localhost:5000/course/apply
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        // localhost:5000/course/apply method:POST
+        // Name=value&Email=value&Phone=value&Confirm=value
+        [HttpPost]
+        public IActionResult Apply(Student student)
+        {
+            // Model Binding
+            // database kayıt
+            Repository.AddStudent(student);
+            return View("Thanks", student);
+        }
+
+        public IActionResult Details()
+        {
+            var course = new Course();
+            course.Name = "Core Mvc Kursu";
+            course.description = "güzel bir kurs";
+            course.isPublished = true;
+
+            return View(course);
+        }
+
+        // localhost:5000/course/list => course/list.cshtml
+        public IActionResult List()
+        {
+          var students = Repository.Students.Where(i=>i.Confirm==true);
+
+            return View(students);
+        }
+    }
 }
